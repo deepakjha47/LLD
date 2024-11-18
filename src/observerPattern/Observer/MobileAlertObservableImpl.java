a8f53117247681ffb631fdc917e246462172d734
Deepak <djha93486@gmail.com>
2024-11-19 00:04:56 +0530

    ObserverPattern

---
+++ src/observerPattern/Observer/MobileAlertObservableImpl.java
@@ -0,0 +1,4 @@
+package observerPattern.Observer;
+
+public class MobileAlertObservableImpl {
+}
---
+++ src/observerPattern/Observer/NotificationAlertObserver.java
@@ -0,0 +1,5 @@
+package observerPattern.Observer;
+
+public interface Observer {
+    public void update();
+}
